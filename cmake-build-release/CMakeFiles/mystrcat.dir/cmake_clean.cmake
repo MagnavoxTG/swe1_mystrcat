7bb401d5d5b856711dba61c55eaf6ee76e424383
MagnavoxTG <dominik.lohner92@gmx.at>
2019-11-22 20:09:34 +0100

    Release 1.0

---
+++ cmake-build-release/CMakeFiles/mystrcat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mystrcat.dir/main.c.o"
+  "mystrcat"
+  "mystrcat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mystrcat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-release/CMakeFiles/mystrcat.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mystrcat.
+# This may be replaced when dependencies are built.
